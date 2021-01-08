8b32bccb1aff1331f18d0b09672902a09efec6af
Codelta <valentin.pretat@he-arc.ch>
2021-01-08 03:14:33 +0100

    terrain de test sur tiled avec plusieurs layers

---
+++ ressources/tiled/collision.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="collision" tilewidth="13" tileheight="13" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="13" height="13" source="../world/collision.png"/>
+ </tile>
+</tileset>
