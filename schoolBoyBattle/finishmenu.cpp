b99c15402ca54da8290d72fdb83de7cfb0dd4fba
kevin.badel <kevin.badel@he-arc.ch>
2021-01-21 22:32:00 +0100

    Menu terminé

--- schoolBoyBattle/finishmenu.cpp
+++ schoolBoyBattle/finishmenu.cpp
@@ -3,7 +3,7 @@
 
 FinishMenu::FinishMenu(QWidget *parent) : QWidget(parent)
 {
-    int victoire=0;
+    int victoire=1;
 
 
     QPalette pal = palette();
@@ -22,17 +22,17 @@ FinishMenu::FinishMenu(QWidget *parent) : QWidget(parent)
      QLabel *imgVictory=new QLabel;
      if(victoire)
      {
-         btnReturnMenu->setStyleSheet("QPushButton {background-color: black; color: white;font-family:Algerian; font-size:12pt;}");
+         btnReturnMenu->setStyleSheet("QPushButton {background-color: Black; color: white;font-family:Algerian; font-size:12pt;}");
          labelInfos->setText("The black winner");
-         labelInfos->setStyleSheet("QLabel { color: black; font-size:36pt;font-family:Algerian;  }");
+         labelInfos->setStyleSheet("QLabel { color: black; font-size:36pt;  }");
          QPixmap pixmapVictory(":/Resources/brand/black.png");
          imgVictory->setPixmap(pixmapVictory);
      }
      else
      {
-         btnReturnMenu->setStyleSheet("QPushButton {background-color: red; color: white;font-family:Algerian; font-size:12pt;}");
+         btnReturnMenu->setStyleSheet("QPushButton {background-color: Red; color: white;font-family:Algerian; font-size:12pt;}");
          labelInfos->setText("The red winner");
-         labelInfos->setStyleSheet("QLabel { color: red; font-size:36pt;font-family:Algerian;  }");
+         labelInfos->setStyleSheet("QLabel { color: red; font-size:36pt;  }");
          QPixmap pixmapVictory(":/Resources/brand/red.png");
          imgVictory->setPixmap(pixmapVictory);
      }
@@ -46,11 +46,12 @@ FinishMenu::FinishMenu(QWidget *parent) : QWidget(parent)
      btnLayout->addStretch(1);
      btnLayout->addWidget(btnReturnMenu);
      btnLayout->addStretch(1);
-     //vLayout->addStretch(1);
+     vLayout->addStretch(1);
      vLayout->addLayout(victoryLayout);
      vLayout->addLayout(labelLayout);
+     vLayout->addStretch(1);
      vLayout->addLayout(btnLayout);
-    // vLayout->addStretch(1);
+     vLayout->addStretch(1);
      mainLayout->addStretch(1);
      mainLayout->addLayout(vLayout);
      mainLayout->addStretch(1);
